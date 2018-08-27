a2506df1c803de001aa9e52a5e5c34d1c21dfad4
FengShixiang <f1992863292@outlook.com>
2018-08-27 10:09:25 +0800

    test

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    var alertX = document.querySelector(`#alertX`)
+    if ((!navigator.getUserMedia) && (!navigator.mediaDevices.getUserMedia)) {
+        alertX.textContent = `no`
+    } else {
+        alertX.textContent = `yes`
+    }
+    alertX.textContent += navigator.userAgent
+}
+window.alert(`JSrun`)
+document.write(`JSrun`)
